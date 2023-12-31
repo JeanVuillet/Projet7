2eba2995062be22804264763b22f04bea3ac0e2a
Jean Vuillet <vuillet433@hotmail.com>
2023-12-31 13:44:51 -0500

    ok

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+import{ alerter } from "./listMaker.js";
+
 const finders = document.getElementsByClassName("finders__option");
 
 // Utiliser forEach sur la collection pour itérer sur chaque élément
 Array.from(finders).forEach(element => {
-    element.addEventListener("click",()=>  element.classList.toggle('active'))
+    element.addEventListener("click",()=>  element.classList.add('active'))
 
 });
+
+//including finders content
+const ingrédientDiv= document.getElementById("finders__ingrédients");
+
+alerter();
---
+++ listMaker.js
@@ -0,0 +1,18 @@
+import {recipes} from "./recipes.js";
+
+export  function alerter(){
+//sort ingredients
+
+let listIngredients=[];
+
+for(let i=0; i<recipes.length; i++){
+    for(let j=0; j<recipes[i].ingredients.length; j++){
+        listIngredients.push(recipes[i].ingredients[j].ingredient);
+
+    }
+}
+
+listIngredients=listIngredients.sort( function (a,b){if(a>b){return 1} else{ return -1} });
+
+console.log(listIngredients);
+};
