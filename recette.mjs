8cf91c3e8b90f30f6ae37ce1c10f1de7292feaac
Jean Vuillet <vuillet433@hotmail.com>
2024-02-06 10:33:30 -0500

    all images ok prettyer on recette.mjs

--- recette.mjs
+++ recette.mjs
@@ -1,93 +1,96 @@
+export default function recetteMaker(recette) {
+  //creating container
+  const recetteContainer = document.createElement("article");
+  recetteContainer.className = "recetteContainer";
 
-export default function recetteMaker( recette){
-    //creating container
-const recetteContainer=document.createElement("article");
-recetteContainer.className="recetteContainer";
+  //creating image
+  const imageRecette = document.createElement("img");
+  imageRecette.src = `Assets/${recette.image}`;
+  imageRecette.alt = `${recette.name}`;
+  imageRecette.className = "recetteContainer__imageRecette";
+  recetteContainer.appendChild(imageRecette);
+  // creating details Div
+  const detailDiv = document.createElement("div");
+  detailDiv.className = "recetteContainer__detail";
+  //creating titile
+  const title = document.createElement("h2");
+  WebTransportBidirectionalStream.className = "recetteContainer__detail__title";
+  title.innerText = recette.name;
+  detailDiv.appendChild(title);
 
-//creating image
-const imageRecette=document.createElement("img");
-imageRecette.src=`Assets/${recette.image}` 
-imageRecette.alt=`${recette.name}`
-imageRecette.className="recetteContainer__imageRecette";
-recetteContainer.appendChild(imageRecette);
-// creating details Div
-const detailDiv=document.createElement('div');
-detailDiv.className='recetteContainer__detail';
-//creating titile
-const title=document.createElement("h2");
-WebTransportBidirectionalStream.className="recetteContainer__detail__title";
-title.innerText=recette.name;
-detailDiv.appendChild(title);
+  //creating recepe section
+  const recepeSection = document.createElement("section");
+  recepeSection.className = "recetteContainer__detail__recepeSection";
+  //creating title
+  const recepeSectionTitle = document.createElement("h3");
+  recepeSectionTitle.innerText = "Recette";
+  recepeSectionTitle.className =
+    "recetteContainer__detail__recepeSection__title";
+  detailDiv.appendChild(recepeSectionTitle);
+  //creating timeDiv
+  const recetteSectionTime = document.createElement("div");
+  recetteSectionTime.className = "recetteContainer__detail__time";
+  recetteSectionTime.innerText = recette.time + "min";
+  detailDiv.appendChild(recetteSectionTime);
 
-//creating recepe section
-const recepeSection=document.createElement("section");
-recepeSection.className="recetteContainer__detail__recepeSection";
-//creating title
-const recepeSectionTitle=document.createElement("h3");
-recepeSectionTitle.innerText="Recette";
-recepeSectionTitle.className="recetteContainer__detail__recepeSection__title";
-detailDiv.appendChild(recepeSectionTitle);
-//creating timeDiv
-const recetteSectionTime=document.createElement('div');
-recetteSectionTime.className="recetteContainer__detail__time";
-recetteSectionTime.innerText=recette.time+'min';
-detailDiv.appendChild(recetteSectionTime);
+  //creating description
+  const recepe = document.createElement("p");
+  recepe.className = "recetteContainer__detail__recepeSection__recepe";
+  recepe.innerText = recette.description;
+  detailDiv.appendChild(recepe);
 
-//creating description
-const recepe=document.createElement("p");
-recepe.className="recetteContainer__detail__recepeSection__recepe";
-recepe.innerText=recette.description;
-detailDiv.appendChild(recepe);
+  //creating ingredients section
 
-//creating ingredients section
+  const ingredientsSection = document.createElement("section");
+  ingredientsSection.className = "recetteContainer__detail__ingredientsSection";
+  //creating title
+  const ingredientsSectionTitle = document.createElement("h3");
+  ingredientsSectionTitle.innerText = "Ingrédients";
+  ingredientsSectionTitle.className =
+    "recetteContainer__detail__ingredientsSection__title";
 
-const ingredientsSection=document.createElement("section");
-ingredientsSection.className="recetteContainer__detail__ingredientsSection";
-//creating title
-const ingredientsSectionTitle=document.createElement("h3");
-ingredientsSectionTitle.innerText="Ingrédients";
-ingredientsSectionTitle.className="recetteContainer__detail__ingredientsSection__title";
-
-//creating content
-const ingredients=document.createElement("div");
-ingredients.className="recetteContainer__detail__ingredientsSection__ingredients";
-// implementing title
-detailDiv.appendChild(ingredientsSectionTitle);
-//implementing ingredients
-recette.ingredients.forEach(element => {
-    const ingredientDiv=document.createElement("div");
-    ingredientDiv.className="recetteContainer__detail__ingredientsSection__ingredients__nameQuantity";
+  //creating content
+  const ingredients = document.createElement("div");
+  ingredients.className =
+    "recetteContainer__detail__ingredientsSection__ingredients";
+  // implementing title
+  detailDiv.appendChild(ingredientsSectionTitle);
+  //implementing ingredients
+  recette.ingredients.forEach((element) => {
+    const ingredientDiv = document.createElement("div");
+    ingredientDiv.className =
+      "recetteContainer__detail__ingredientsSection__ingredients__nameQuantity";
     //name
     const ingredient = document.createElement("p");
-    ingredient.className="recetteContainer__detail__ingredientsSection__ingredients__nameQuantity__name";
+    ingredient.className =
+      "recetteContainer__detail__ingredientsSection__ingredients__nameQuantity__name";
     ingredient.innerText = element.ingredient;
     ingredientDiv.appendChild(ingredient);
     //quantity and unit
     const quantity = document.createElement("p");
-    quantity.className="recetteContainer__detail__ingredientsSection__ingredients__nameQuantity__quantity";
-    if((element.quantity)&&(element.unit)){
-        quantity.innerText = element.quantity+element.unit }
-      else if((element.unit==undefined)&&(element.quantity))
-      { quantity.innerText=element.quantity }
-      //il n y a ni quntite ni unite
-      else{quantity.innerText=''};
+    quantity.className =
+      "recetteContainer__detail__ingredientsSection__ingredients__nameQuantity__quantity";
+    if (element.quantity && element.unit) {
+      quantity.innerText = element.quantity + element.unit;
+    } else if (element.unit == undefined && element.quantity) {
+      quantity.innerText = element.quantity;
+    }
+    //il n y a ni quntite ni unite
+    else {
+      quantity.innerText = "";
+    }
     //no name and no unit
-    
-
 
     //name
     const ingredientName = document.createElement("p");
     ingredientDiv.appendChild(quantity);
     ingredients.appendChild(ingredientDiv);
+  });
+  //implementing ingredients
+  ingredientsSection.appendChild(ingredients);
+  //implementing ingredient section in article
+  detailDiv.appendChild(ingredientsSection);
+  recetteContainer.appendChild(detailDiv);
 
-    
-});
-//implementing ingredients
-ingredientsSection.appendChild(ingredients);
-//implementing ingredient section in article
-detailDiv.appendChild(ingredientsSection);
-recetteContainer.appendChild(detailDiv);
-
-return recetteContainer;
-
+  return recetteContainer;
 }
